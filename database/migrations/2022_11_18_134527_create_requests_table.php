10081845331e2fc5aeab39c2dff7d5bc0a657a83
LeonardoC <95712634+LeonardoCoaquira@users.noreply.github.com>
2022-11-18 10:45:58 -0500

    s2

---
+++ database/migrations/2022_11_18_134527_create_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('book_title');
+            $table->string('request_content');
+            $table->string('author');
+            $table->id('date_published');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+};
